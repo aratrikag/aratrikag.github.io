f0fdcb234accfa6c59fa923de3d89eec68e334a3
aratrikag <aratrikag384@gmail.com>
2019-06-10 21:04:37 -0700

    Update Accomplishments.js

--- src/components/Accomplishments.js
+++ src/components/Accomplishments.js
@@ -4,99 +4,71 @@ import Skills from "./Skills";
 class Accomplishments extends Component {
   render() {
     return (
-      <div>
-        <Grid>
-          <Cell col={4}>
-            <p>2015 - 2018</p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{ marginTop: "0px" }}>Girls Who Code Ambassador</h4>
-            <p>
-              <ul>
-                <li>
-                  I helped students learn how to make websites, application and
-                  code in various languages such as Python and Java.
-                </li>
-                <li>I assisted instructors in lesson planning and teaching</li>
-              </ul>
-            </p>
-          </Cell>
-          <Cell col={4}>
-            <p>2016 - 2018</p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{ marginTop: "0px" }}>Tutor at Silver Creek Academy</h4>
-            <p>
-              <ul>
-                <li>
-                  I tutored and assistd children ages 7-14 in several subjects
-                  including math, english, and public speaking
-                </li>
-              </ul>
-            </p>
-          </Cell>
-          <Cell col={4}>
-            <p>2015 - 2018</p>
-          </Cell>
-          <Cell col={8}>
-            <h4 style={{ marginTop: "0px" }}>
-              Instructor at ‘An Appetite for Apps’
+      <Grid className="accomB">
+        <div className="accomB">
+          <div className="gwc" col={8}>
+            <h4>
+              <b>Girls Who Code Ambassador</b>: 2016-2018
             </h4>
             <p>
+              I helped students learn how to make websites, application and code
+              in various languages such as Python and Java. In addition, I
+              assisted instuctors in lesson planning and teaching.
+            </p>
+          </div>
+
+          <div className="gwc1" col={8}>
+            <h4>
+              {" "}
+              <b>Tutor at Silver Creek Academy</b>: 2016-2018
+            </h4>
+            <p>
+              I assisted the teachers in managing classes and students, and
+              helped teach subjects such as math, english, and public speaking.
+            </p>
+          </div>
+          <div col={4} />
+
+          <div className="gwc" col={8}>
+            <h4>
+              <b>Instructor at ‘An Appetite for Apps’</b>: 2016-2018
+            </h4>
+            <p>
+              I cofounded a program that taught children in elementary and
+              middle schools how to create applications using MIT app inverntor
+              2. I gave weekly lessons on mobile application creation, allowing
+              the kids to test and develop their apps.
+            </p>
+          </div>
+          <div className="gwc1">
+            <p>
+              <h4>
+                <b>Skills Include:</b>
+              </h4>
               <ul>
-                <li>
-                  I cofounded a program that taught children in elementary and
-                  middle schools how to create applications using MIT app
-                  inverntor 2.
-                </li>
-                <li>
-                  I gave weekly lessons on mobile application creation, allowing
-                  the kids to test and develop their apps.
-                </li>
+                <h4>
+                  <li>Python(Proficient)</li>
+                  <li>Java(Proficient)</li>
+                  <li>Swift(Beginner)</li>
+                  <li>JS/HTML/CSS(Beginner)</li>
+                </h4>
               </ul>
             </p>
-          </Cell>
-          />
-          <hr style={{ borderTop: "3px solid #e22947" }} />
-          <h2>Skills</h2>
-          <Cell col={12}>
-            <div style={{ display: "flex" }}>
-              <h4>Java: Proficient</h4>
-              <ProgressBar
-                style={{ margin: "auto", width: "75%" }}
-                progress={74}
-              />{" "}
-            </div>
-          </Cell>
-          <Cell col={12}>
-            <div style={{ display: "flex" }}>
-              <h4>Python: Proficient</h4>
-              <ProgressBar
-                style={{ margin: "auto", width: "75%" }}
-                progress={70}
-              />{" "}
-            </div>
-          </Cell>
-          <Cell col={12}>
-            <div style={{ display: "flex" }}>
-              <h4>Swift: Beginner</h4>
-              <ProgressBar
-                style={{ margin: "auto", width: "75%" }}
-                progress={20}
-              />{" "}
-            </div>
-          </Cell>
-          <Cell col={12}>
-            <div style={{ display: "flex" }}>
-              <h4>JS/HTML/CSS: Beginner</h4>
-              <ProgressBar
-                style={{ margin: "auto", width: "75%" }}
-                progress={20}
-              />{" "}
-            </div>
-          </Cell>
-        </Grid>
-      </div>
+          </div>
+          <div col={10}>
+            <img
+              src="https://anappetiteforapps.files.wordpress.com/2016/05/img_0270.jpg?w=1556"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+            <img
+              src="https://files.constantcontact.com/711f5ea9001/6bed921c-1136-4658-829f-6e0793708353.jpg?a=1126186292633"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+          </div>
+        </div>
+      </Grid>
     );
   }
 }
