fc32c2637e8ef56b49838270dab3ddeaa62ee1fb
aratrikag <aratrikag384@gmail.com>
2019-06-10 21:07:09 -0700

    Update landingPage.js

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
-class landingPage extends Component {
+class Contact extends Component {
   render() {
     return (
-      <div style={{ width: "100%", margin: "auto" }}>
-        <Grid className="landing-grid">
-          <Cell col={12}>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Aratrika Ghatak</h2>
             <img
               src="https://lh3.googleusercontent.com/p4HYuw_7_vBy1RezbDhwbxabmaDkBknuGwKuVjVMXv1biWNwk68bhDSeDdA8rDE3QRIG1hhXIZNYzbZ_mzhyxRLGfLB5OZhRk70dX7dTr71D0CWiOaQST5PQxZRyeRYZBkHnTA2R=w2400"
-              alt="me"
-              className="me-img"
+              alt="avatar"
+              style={{ height: "400px" }}
             />
-            <div className="banner-text">
-              <h1>Aratrika Ghatak's Resume</h1>
-              <h3>
-                I am currently a rising sophomore at Cornell University. I am
-                double majoring in Computer Science and Electrical and Computer
-                Engineering, with a minor in business.{" "}
-              </h3>
-              <hr />
+            <p style={{ width: "75%", margin: "auto" }}>
+              Feel free to contact me at any of the following:
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: "30px" }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (408) 800-8131
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{ fontSize: "30px" }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    ag988@cornell.edu
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: "30px" }}>
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    MySkypeID: aratrikag
+                  </ListItemContent>
+                </ListItem>
+              </List>
             </div>
           </Cell>
         </Grid>
@@ -27,4 +49,5 @@ class landingPage extends Component {
     );
   }
 }
-export default landingPage;
+export default Contact;
+
