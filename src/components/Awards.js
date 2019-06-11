4147a23c85c4adc828f05cfd9cac47f3f165f4f7
aratrikag <aratrikag384@gmail.com>
2019-06-10 21:05:18 -0700

    Update Awards.js

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -23,88 +23,87 @@ class Awards extends Component {
     if (this.state.activeTab === 0) {
       return (
         <div className="projects-grid">
-          {/* Project 1 */}
-          <Card shadow={5} style={{ minWidth: "600", margin: "auto" }}>
-            <CardTitle>CS 2110(Object oriented programming)</CardTitle>
-            <CardText>
-              I completed various projects using JAVA, implementing heaps,
-              binary trees, linked lists, and graphs.
-            </CardText>
-            <CardActions border />
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
+          <Cell col={4}>
+            {/* Project 1 */}
+            <Card
+              className="cards"
+              shadow={5}
+              style={{ minWidth: "1000", margin: "20", width: "700" }}
+            >
+              <CardTitle>CS 2110(Object oriented programming)</CardTitle>
+              <CardText>
+                I completed various projects using JAVA, implementing heaps,
+                binary trees, linked lists, and graphs.
+              </CardText>
+              <CardActions border />
+            </Card>
+          </Cell>
+          <Cell col={4}>
+            {/* Project 2 */}
+            <Card shadow={5} style={{ minWidth: "450", margin: "20" }}>
+              <CardTitle>
+                Machine Learning to Predict Safety Rates of College Campuses
+              </CardTitle>
+              <CardText>
+                ​In this project, I developed a safety grading system of US
+                college campuses. I used college campus crime statistics from US
+                Dept. of Education, and collected related attributes from
+                various governmental websites such as FBI, Dept. of Labor, US
+                Census Bureau etc. Then I ran various Machine Learning
+                algorithms using the platform ‘Weka’ on the training dataset of
+                approximately 1.5M data points to find out the best predictive
+                model.
+              </CardText>
+              <CardActions border>
+                <a href="https://www.youtube.com/watch?v=lVK20ryqmJA">
+                  <button>Live Demo</button>
+                </a>
+              </CardActions>
+            </Card>
+          </Cell>
+          <Cell col={4}>
+            {/* Project 3 */}
+            <Card shadow={5} style={{ minWidth: "450", margin: "20" }}>
+              <CardTitle>
+                Traffic Signal Preemption for Emergency Vehicles
+              </CardTitle>
+              <CardText>
+                I prototyped a low cost traffic preemption solution for
+                emergency vehicles using mobile applications. I developed the
+                applications on MIT App-Inventor 2 for Android utilizing Google
+                drive and Fusion table APIs.
+              </CardText>
+              <CardActions border>
+                <a href="https://www.youtube.com/watch?v=qbNUGLXbZsc">
+                  <button>Live Demo</button>
+                </a>
+              </CardActions>
+            </Card>
+          </Cell>
+          <Cell col={8}>
+            {/* Project 1 */}
+            <Card shadow={5} style={{ minWidth: "600", margin: "20" }}>
+              <CardTitle
+                style={{
+                  color: "#fff",
+                  height: "350px",
+                  background:
+                    "url(https://lh3.googleusercontent.com/-h81qApom7v5W5VuNFj7ZcLiIqB6AjhAVimKC_2PB807PkJypCVpUU6A-xslcSDRq1e5fWAYYIukH2p0h-StLF69xmEqHLcnP-nzuCJHapbh2-MRjRhGo0B5Idj3rLgWoJYZVCVd=w2400) center / cover"
+                }}
+              />
+              <CardTitle>Intel ISEF</CardTitle>
+              <CardText>
+                I was an Intel ISEF finalist for my project that predicted
+                safety rates for different colleges using machine learning.
+              </CardText>
+              <CardActions border />
+              <CardMenu style={{ color: "#fff" }}>
+                <IconButton name="share" />
+              </CardMenu>
+            </Card>
+          </Cell>
           {/* Project 2 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-            <CardTitle>
-              Machine Learning to Predict Safety Rates of College Campuses
-            </CardTitle>
-            <CardText>
-              ​In this project, I developed a safety grading system of US
-              college campuses. I used college campus crime statistics from US
-              Dept. of Education, and collected related attributes from various
-              governmental websites such as FBI, Dept. of Labor, US Census
-              Bureau etc. Then I ran various Machine Learning algorithms using
-              the platform ‘Weka’ on the training dataset of approximately 1.5M
-              data points to find out the best predictive model.
-            </CardText>
-            <CardActions border>
-              <a href="https://www.youtube.com/watch?v=lVK20ryqmJA">
-                <button>Live Demo</button>
-              </a>
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-          {/* Project 3 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-            <CardTitle>
-              Traffic Signal Preemption for Emergency Vehicles
-            </CardTitle>
-            <CardText>
-              I prototyped a low cost traffic preemption solution for emergency
-              vehicles using mobile applications. I developed the applications
-              on MIT App-Inventor 2 for Android utilizing Google drive and
-              Fusion table APIs.
-            </CardText>
-            <CardActions border>
-              <a href="https://www.youtube.com/watch?v=qbNUGLXbZsc">
-                <button>Live Demo</button>
-              </a>
-            </CardActions>
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-        </div>
-      );
-    } else if (this.state.activeTab === 1) {
-      return (
-        <div className="awards-grid">
-          {/* Project 1 */}
-          <Card shadow={5} style={{ minWidth: "600", margin: "auto" }}>
-            <CardTitle
-              style={{
-                color: "#fff",
-                height: "350px",
-                background:
-                  "url(https://lh3.googleusercontent.com/-h81qApom7v5W5VuNFj7ZcLiIqB6AjhAVimKC_2PB807PkJypCVpUU6A-xslcSDRq1e5fWAYYIukH2p0h-StLF69xmEqHLcnP-nzuCJHapbh2-MRjRhGo0B5Idj3rLgWoJYZVCVd=w2400) center / cover"
-              }}
-            />
-            <CardTitle>Intel ISEF</CardTitle>
-            <CardText>
-              I was an Intel ISEF finalist for my project that predicted safety
-              rates for different colleges using machine learning.
-            </CardText>
-            <CardActions border />
-            <CardMenu style={{ color: "#fff" }}>
-              <IconButton name="share" />
-            </CardMenu>
-          </Card>
-          {/* Project 2 */}
-          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <Card shadow={5} style={{ minWidth: "450", margin: "20" }}>
             <CardTitle>Technovation Semifinalist</CardTitle>
             <CardText>
               ​ I organized and led a team to semifinals in a mobile application
@@ -145,3 +144,4 @@ class Awards extends Component {
   }
 }
 export default Awards;
+
