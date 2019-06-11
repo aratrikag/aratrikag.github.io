3451b0a693e85420a2b7a0c6adc664ed210da18c
aratrikag <aratrikag384@gmail.com>
2019-06-10 21:17:13 -0700

    Update landingPage.js

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,47 +1,47 @@
 import React, { Component } from "react";
-import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import { Grid, Cell } from "react-mdl";
 
-class Contact extends Component {
+class landingPage extends Component {
   render() {
     return (
-      <div className="contact-body">
-        <Grid className="contact-grid">
-          <Cell col={6}>
-            <h2>Aratrika Ghatak</h2>
-            <img
-              src="https://lh3.googleusercontent.com/p4HYuw_7_vBy1RezbDhwbxabmaDkBknuGwKuVjVMXv1biWNwk68bhDSeDdA8rDE3QRIG1hhXIZNYzbZ_mzhyxRLGfLB5OZhRk70dX7dTr71D0CWiOaQST5PQxZRyeRYZBkHnTA2R=w2400"
-              alt="avatar"
-              style={{ height: "400px" }}
-            />
-            <p style={{ width: "75%", margin: "auto" }}>
-              Feel free to contact me at any of the following:
-            </p>
-          </Cell>
-          <Cell col={6}>
-            <h2>Contact Me</h2>
-            <hr />
-            <div className="contact-list">
-              <List>
-                <ListItem>
-                  <ListItemContent style={{ fontSize: "30px" }}>
-                    <i className="fa fa-phone-square" aria-hidden="true" />
-                    (408) 800-8131
-                  </ListItemContent>
-                </ListItem>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <div className="banner-text">
+              <h1>Hi. My name is Aratrika Ghatak</h1>
+              <h3>
+                I am currently a rising sophomore at Cornell University. I am
+                double majoring in Computer Science and Electrical and Computer
+                Engineering, with a minor in business.{" "}
+              </h3>
+              <hr />
 
-                <ListItem>
-                  <ListItemContent style={{ fontSize: "30px" }}>
-                    <i className="fa fa-envelope" aria-hidden="true" />
-                    ag988@cornell.edu
-                  </ListItemContent>
-                </ListItem>
-                <ListItem>
-                  <ListItemContent style={{ fontSize: "30px" }}>
-                    <i className="fa fa-skype" aria-hidden="true" />
-                    MySkypeID: aratrikag
-                  </ListItemContent>
-                </ListItem>
-              </List>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/aratrika-ghatak-388809155"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* Github */}
+                <a
+                  href="https://github.com/aratrikag/aratrikag.github.io"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/* Youtube */}
+                <a
+                  href="https://www.youtube.com/playlist?list=UUzGk8mLb9sFQuJiDNyXdUcA"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+              </div>
             </div>
           </Cell>
         </Grid>
@@ -49,5 +49,6 @@ class Contact extends Component {
     );
   }
 }
-export default Contact;
+export default landingPage;
+
 
